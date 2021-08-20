97cda2a98a06c2b3bf83653d8cb631314e58e641
Santi <santideparis@gmail.com>
2021-08-20 16:53:21 +0800

    paste solution from educative.io

--- product_of_array_except_self.py
+++ product_of_array_except_self.py
@@ -16,5 +16,20 @@ def find_product(lst):
             else:
                 l.append(0)
         else:
-            l.append(product / i)
+            l.append(product // i)
     return l
+
+# def find_product(lst):
+#     # get product start from left
+#     left = 1
+#     product = []
+#     for ele in lst:
+#         product.append(left)
+#         left = left * ele
+#     # get product starting from right
+#     right = 1
+#     for i in range(len(lst)-1, -1, -1):
+#         product[i] = product[i] * right
+#         right = right * lst[i]
+
+#     return product
