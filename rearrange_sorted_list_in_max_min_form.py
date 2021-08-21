f1d1e55d8ff8b317f066fe35d62000191a6907a8
SantiDu <santideparis@gmail.com>
2021-08-21 21:29:53 +0800

    store two numbers at one index, can't deal with negative values

--- rearrange_sorted_list_in_max_min_form.py
+++ rearrange_sorted_list_in_max_min_form.py
@@ -22,4 +22,18 @@ def max_min2(lst):
             quotient -= 1
     return l
 
-# def max_min3(lst):
+def max_min3(lst):
+    def two_numbers_in_one():
+        n_to_hold = lst[index] % n_max_pp
+        return n + n_to_hold * n_max_pp
+    n_max_pp = lst[-1] + 1
+    for i, n in enumerate(lst):
+        if i % 2 == 0:
+            index = len(lst) - (i // 2 + 1)
+            lst[i] = two_numbers_in_one()
+        else:
+            index = (i - 1) // 2
+            lst[i] = two_numbers_in_one()
+    for i, n in enumerate(lst):
+        lst[i] //= n_max_pp
+    return lst
