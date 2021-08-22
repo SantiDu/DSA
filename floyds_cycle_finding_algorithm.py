39c75e534b8462f6e1700e5b52fd05b96f060298
SantiDu <santideparis@gmail.com>
2021-08-22 17:37:46 +0800

    80% rt, 93% mu leetcode

---
+++ floyds_cycle_finding_algorithm.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+def hasCycle(self, head: ListNode) -> bool:
+    ptr1 = ptr2 = head
+    while ptr2 and ptr2.next: 
+        ptr1 = ptr1.next
+        ptr2 = ptr2.next.next
+        if ptr1 == ptr2:
+            return True
+    return False
