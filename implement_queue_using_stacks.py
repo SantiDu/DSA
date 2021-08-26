852b6c7c94229a7c0faeff83bbe47423b5c25d3b
SantiDu <santideparis@gmail.com>
2021-08-26 15:02:21 +0800

    solution 2, 11%rt 74mu leetcode

--- implement_queue_using_stacks.py
+++ implement_queue_using_stacks.py
@@ -79,7 +79,51 @@ class MyQueue1:
         """
         return self.queue.is_empty()
 
+    
+class MyQueue2:
 
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.queue = Stack()
+        self.second_stack = Stack()
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        while not self.queue.is_empty():
+            self.second_stack.push(self.queue.pop())
+        self.queue.push(x)
+        while not self.second_stack.is_empty():
+            self.queue.push(self.second_stack.pop())
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        return self.queue.pop()
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        return self.queue.peek()
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return self.queue.is_empty()
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
 # obj.push(x)
