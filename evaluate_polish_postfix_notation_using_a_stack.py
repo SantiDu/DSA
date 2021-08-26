2ed78fcb8092ff454f3631e715abf82909cf493b
SantiDu <santideparis@gmail.com>
2021-08-26 16:52:56 +0800

    5% rt, 48% mu leetcode

---
+++ evaluate_polish_postfix_notation_using_a_stack.py
@@ -0,0 +1,14 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for i in tokens:
+            try:
+                stack.append(int(i))
+            except ValueError:
+                rhs = stack.pop()
+                lhs = stack.pop()
+                v = eval(f"{lhs}{i}{rhs}")
+                if i == "/":
+                    v = int(v)
+                stack.append(v)
+        return stack[0]
