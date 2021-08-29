416bdd7c33f1981b18b647dad433dc679a49c589
SantiDu <santideparis@gmail.com>
2021-08-29 22:32:56 +0800

    leetcode, rt 93%, mu 85%

---
+++ Stack-next_greater_element_2.py
@@ -0,0 +1,29 @@
+from math import inf
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        maximum = -inf
+        for i, n in enumerate(nums):
+            if n > maximum:
+                maximum = n
+                max_i = i
+        i = (max_i - 1) % len(nums)
+        next_greaters = [maximum]
+        arg_max = [max_i]
+        for j in range(len(nums) - 1):
+            curr = nums[i]
+            if curr < next_greaters[-1]:
+                nums[i] = next_greaters[-1]
+                next_greaters.append(curr)
+            else:
+                while curr >= next_greaters[-1]:
+                    if curr != maximum:
+                        next_greaters.pop()
+                    else:
+                        arg_max.append(i)
+                        break
+                nums[i] = next_greaters[-1]
+                next_greaters.append(curr)
+            i -= 1
+        for i in arg_max:
+            nums[i] = -1
+        return nums
