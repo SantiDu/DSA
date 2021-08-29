a318f54aa353d1b2017fd01faa15e47dba6df68e
SantiDu <santideparis@gmail.com>
2021-08-29 22:36:53 +0800

    leetcode, rt 85%, mu 64%

---
+++ Stack-valid_parentheses.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for i in s:
+            if i == ")":
+                if stack and stack[-1] == "(":
+                    stack.pop()
+                else:
+                    return False
+            elif i == "]":
+                if stack and stack[-1] == "[":
+                    stack.pop()
+                else:
+                    return False
+            elif i == "}":
+                if stack and stack[-1] == "{":
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(i)
+        if stack:
+            return False
+        else:
+            return True
