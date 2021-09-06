b01837a332e99e2f519eaf62f328261064423765
Santi <santideparis@gmail.com>
2021-09-06 19:00:28 +0800

    educative

---
+++ BST-nth_largest_element_in_a_bst.py
@@ -0,0 +1,23 @@
+def find_nth_highest_in_bst(node, n):
+    if node is None:
+        return None
+      
+    k = 0
+    stack = []
+    
+    def go_right(node):
+        while node:
+            stack.append(node)
+            node = node.right
+            
+            
+    go_right(node)
+
+    while stack:
+        node = stack.pop()
+        k += 1
+        if k == n:
+            return node
+        go_right(node.left)
+    
+    return None
